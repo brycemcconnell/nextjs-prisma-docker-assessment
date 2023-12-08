5ff1a20ca7a2e90c6c9eef230350645844f20e48
Bryce McConnell <32624581+brycemcconnell@users.noreply.github.com>
2023-12-08 12:58:26 +1100

    add machine numbers

--- app/api/batch/route.ts
+++ app/api/batch/route.ts
@@ -3,14 +3,23 @@ import { prisma } from '@/lib/prisma'
 
 export async function POST(request: Request) {
   const data = await request.json()
-  console.log(data)
+  
+  const model = data.model
+  const quantity = Number(data.quantity)
+  const date = (new Date(data.date)).toISOString()
+
   const batch = await prisma.batch.create({
     data: {
-      model: data.model,
-      quantity: Number(data.quantity),
-      date: (new Date(data.date)).toISOString(),
+      model,
+      date,
       license_level: Number(data.licenseLevel),
-      comment: data.comment
+      comment: data.comment,
+      machine_numbers: {
+        create: Array(quantity).fill({
+          model,
+          date
+        })
+      }
     }
   })  
   return NextResponse.json({batch})
--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -2,12 +2,24 @@
 CREATE TABLE "Batch" (
     "id" SERIAL NOT NULL,
     "model" TEXT NOT NULL,
-    "quantity" INTEGER NOT NULL,
     "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "license_level" INTEGER NOT NULL,
-    "serial_number" TEXT NOT NULL,
     "comment" TEXT,
 
     CONSTRAINT "Batch_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "Machine_Number" (
+    "id" SERIAL NOT NULL,
+    "batch_id" INTEGER NOT NULL,
+    "model" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "serial_number" TEXT NOT NULL,
+
+    CONSTRAINT "Machine_Number_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Machine_Number" ADD CONSTRAINT "Machine_Number_batch_id_fkey" FOREIGN KEY ("batch_id") REFERENCES "Batch"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
